3e081d2d70d5e92ad6d8384e205d0a1bc1c2b930
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-17 22:21:44 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -63,7 +63,7 @@ for i in range(15):
 coffees_monthly = st.sidebar.checkbox("Coffees per month")
 if coffees_monthly:
     st.header("Coffees per month per person")
-    df = pd.DataFrame(monthly_coffees1, columns=names, months)
+    df = pd.DataFrame(monthly_coffees1, months, columns=names)
     st.write(alt.Chart(df).mark_bar().encode(x=alt.X('months', sort=None),y='monthly_coffees1',))
     #sorterIndex = dict(zip(months, range(len(months))))
     #df['month_Rank'] = df['months'].map(sorterIndex)
