a945848d8d90b1017edeb83b341f98a001bb7a88
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-18 14:40:19 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -76,7 +76,7 @@ if coffees_monthly:
       
       
       
-    fig = px.scatter(df, y="number of coffees", title="Number of coffees per month per person")
+    fig = px.scatter(df, title="Number of coffees per month per person")
     st.plotly_chart(fig)
        
     
