2ec2f94892e25257e2ff7f7308854ed5b5cbd4fd
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-18 14:34:24 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -76,7 +76,7 @@ if coffees_monthly:
       
       
       
-    fig = px.line(df)#,values = df,textinfo = "value"))
+    fig = px.scatter(df)#,values = df,textinfo = "value"))
     st.plotly_chart(fig)
        
     
