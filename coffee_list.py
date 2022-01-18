13bab003d0ec9d0453cd6fd7cbb374dc4f12efb3
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-18 22:27:33 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -174,7 +174,7 @@ if correlation_abs:
    df = pd.DataFrame(temp2, columns={'x-values','y-values'})
    col2.dataframe(df)
    fig5 = px.scatter(df, x='x-values', y='y-values', size=temp3)
-   fig5.update_layout(showlegend=False)
+   fig5.update_layout(showlegend=False, yaxis_visible=False, yaxis_showticklabels=False, xaxis_visible=False)#, xaxis_showticklabels=False)
    col1.plotly_chart(fig5, use_container_width=True)
 
    
