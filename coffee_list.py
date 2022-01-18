eb2988123c31fcf489740c22692b3ee28ba48f35
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-18 20:09:30 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -117,7 +117,7 @@ if coffees_total:
         temp1.append(total_coffees[i])
         temp.append(temp1)
     df = pd.DataFrame(temp, columns={"names","total"}, index=names)              #total coffees pie chart
-    fig3 = go.Figure(go.Pie(labels = names, values = total_coffees, sort=False, hole=.5))
+    fig3 = go.Figure(go.Pie(labels = names, values = total_coffees, sort=False, hole=.4))
     col1.plotly_chart(fig3, use_container_width=True)
 
     
