db92c48c12ffa3b7c6c5ad9a162ec8cfff0231e9
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-18 15:26:16 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -77,7 +77,7 @@ if coffees_monthly:
          temp1.append(temp)
     
     st.write(temp1)
-    df = pd.DataFrame(temp1, columns={"","total"}, index=months)              #total coffees per month
+    df = pd.DataFrame(temp1, columns={"","total"})              #total coffees per month
     st.dataframe(df)
     fig2 = px.bar(df, x="month", y="total", title="Total number of coffees per month")
     st.plotly_chart(fig2, use_container_width=True)
