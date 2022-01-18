93145e8496a5fbae5720408af15023bf6c00ac2d
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-18 15:36:14 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -85,7 +85,7 @@ if coffees_monthly:
 coffees_total = st.sidebar.checkbox("Total coffees")
 if coffees_total:
     st.header("Total coffees")
-    df = pd.DataFrame(total_coffees, index=names)
+    df = pd.DataFrame(total_coffees, columns={"total"}, index=names)
     st.dataframe(df)
     fig3 = px.pie(names = names,values = total_coffees, sort=False)
     st.plotly_chart(fig3)
