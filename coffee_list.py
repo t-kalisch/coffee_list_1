69935662b7f460f8369e2ab4ecd0b29cc513b97c
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-18 20:04:17 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -80,7 +80,23 @@ if coffees_monthly:
     df = pd.DataFrame(temp1, columns={"months","total"})              #total coffees per month)
     fig2 = px.bar(df, x="total", y="months", title="Total number of coffees per month", labels={"months":"Number of coffees", "total":""}, text_auto=True)
     st.plotly_chart(fig2, use_container_width=True)
-
+    
+    col1, col2 = st.columns([1,1])
+    temp=[]
+    for i in range(len(months)):
+        temp1=[]
+        temp1.append(months[i])
+        for j in range(len(names)):
+            temp1.append(monthly_ratios[j][i])
+        temp.append(temp1)
+    temp2=[]
+    temp2.append("months")
+    for i in range(len(names)):
+        temp2.append(names[i])
+   
+   df_stack=pd.DataFrame(temp, columns = temp2, index = months)
+   fig4 = px.bar(df_stack, x=names, y = months, barmode = 'stack', labels={"y":"", "value":"percentage", "variable":"drinkers"})
+   col2.plotly_chart(fig4, use_container_width=True)
       
 #-------------------------------------------------------------------------------------------------------------- monthly ratios (stacked bar chart)
 ratio_monthly = st.sidebar.checkbox("Monthly ratios")
